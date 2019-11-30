22b43af295f102ef2d05c19106e2a339f6a2284a
xingmeichen <xingmeichen12@126.com>
2019-11-30 16:42:05 +0800

    swap string

--- base/src/main/java/StringLearn.java
+++ base/src/main/java/StringLearn.java
@@ -9,7 +9,7 @@ public class StringLearn {
     /**
      * 1、== 的结果是true,表示地址相同，也就是指向的是同一个对象
      * */
-    public static void main(String[] args) {
+    public static void equalOrNot() {
         String s1 = "abc";
         String s2 = "abc";
         String s3 = "" + s2;
@@ -26,5 +26,23 @@ public class StringLearn {
         System.out.println(s1 == s6); // false
         System.out.println(s1 == s7); // false
         System.out.println(s1 == s9); // true 容易犯错
+    }
+
+    /**
+     * 该方法的调用不会对方法外的实参造成任何影响，因为当调用这个方法的时候，
+     * 只是将实际参数的值赋值给了形参a, b，然后在方法对a, b进行交换
+     */
+    public static void swap(Integer a, Integer b) {
+        Integer temp = a;
+        a = b;
+        b = temp;
+    }
+
+    public static void main(String[] args) {
+        Integer a = 1;
+        Integer b = 2;
+        System.out.println("before swap: a = " + a + ", b = " + b);
+        swap(a, b);
+        System.out.println("after swap: a = " + a + ", b = " + b);
     }
 }
