82ce85b13a6ca976f5d016200d549d21ff50461c
xingmeichen <xingmeichen12@126.com>
2020-05-10 12:26:46 +0800

    add:load aop by annotation

--- spring/src/main/java/com/mabel/aop/MabelTest.java
+++ spring/src/main/java/com/mabel/aop/MabelTest.java
@@ -1,6 +1,10 @@
 package com.mabel.aop;
 
+import org.springframework.aop.aspectj.annotation.AnnotationAwareAspectJAutoProxyCreator;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
@@ -18,9 +22,25 @@ public class MabelTest {
         System.out.println("Hello World");
     }
 
-    public static void main(String[] args) {
+    public static void loadByXml() {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/META-INF/aop.xml");
         MabelTest bean = applicationContext.getBean(MabelTest.class);
         bean.testAspect();
+    }
+
+    public static void loadByAnnotation() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.register(MabelAspect.class);
+        applicationContext.register(MabelTest.class);
+        applicationContext.register(AnnotationAwareAspectJAutoProxyCreator.class);
+        applicationContext.refresh();
+        MabelTest bean = applicationContext.getBean(MabelTest.class);
+        bean.testAspect();
+        applicationContext.close();
+    }
+
+    public static void main(String[] args) {
+        loadByXml();
+        loadByAnnotation();
     }
 }
