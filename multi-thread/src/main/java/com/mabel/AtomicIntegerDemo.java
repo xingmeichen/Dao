13bca6ba955c3ab3635cca35b48050e9dcf3a0d8
xingmeichen <xingmeichen12@126.com>
2020-09-23 07:38:44 +0800

    bate dace test

---
+++ multi-thread/src/main/java/com/mabel/AtomicIntegerDemo.java
@@ -0,0 +1,36 @@
+package com.mabel;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @project: JavaLearning
+ * @description: 证明 AtomicXX 比synchronized快
+ * @author: Mabel.Chen
+ * @create: 2020-09-19 19:23
+ **/
+public class AtomicIntegerDemo {
+
+    private AtomicInteger count = new AtomicInteger(0);
+
+    public void atomicIncrement() {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            count.incrementAndGet();
+        }
+        System.out.println(System.currentTimeMillis() - start);
+    }
+
+    public synchronized void syncIncrement() {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100000;) {
+            i++;
+        }
+        System.out.println(System.currentTimeMillis() - start);
+    }
+
+    public static void main(String[] args) {
+        AtomicIntegerDemo demo = new AtomicIntegerDemo();
+        demo.atomicIncrement();
+        demo.syncIncrement();
+    }
+}
