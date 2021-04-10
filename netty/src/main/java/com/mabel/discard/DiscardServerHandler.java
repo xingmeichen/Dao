c36352a31b4c69cb3354e9ac921c5eba2da9610d
xingmeichen <xingmeichen12@126.com>
2021-04-10 17:11:46 +0800

    add netty module

---
+++ netty/src/main/java/com/mabel/discard/DiscardServerHandler.java
@@ -0,0 +1,29 @@
+package com.mabel.discard;
+
+import io.netty.buffer.ByteBuf;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * @project: JavaLearning
+ * @description:
+ * @author: Mabel.Chen
+ * @create: 2021-04-10 15:40
+ **/
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // Discard the received data silently.
+        ((ByteBuf) msg).release(); // (3)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ netty/src/main/java/com/mabel/echo/EchoServerHandler.java
@@ -0,0 +1,34 @@
+package com.mabel.echo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * @project: JavaLearning
+ * @description: Handler implementation for the echo server.
+ * @author: Mabel.Chen
+ * @create: 2021-04-10 16:53
+ **/
+@Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
