5c0813b2abd5c9add66a24e074eb76abfb5bbb3a
xingmeichen <xingmeichen12@126.com>
2024-06-27 21:01:20 +0800

    delete testing data

---
+++ dao-spring-ai/src/main/java/com/dao/ai/controller/ChatController.java
@@ -0,0 +1,13 @@
+package com.dao.ai.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @project: Dao
+ * @description:
+ * @author: Mabel.Chen
+ * @create: 2024-06-27
+ **/
+@RestController
+public class ChatController {
+}
